f19095fcc0517a67d4eb8ca07f4ff3430d1add26
Trevor Porter <typorter@godaddy.com>
2016-07-14 12:52:55 -0700

    Initial Commit

---
+++ SendSMS/SendSMS.h
@@ -0,0 +1,13 @@
+//
+//  SendSMS.h
+//  SendSMS
+//
+//  Created by Trevor Porter on 7/14/16.
+//  Copyright © 2016 Trevor Porter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SendSMS : NSObject
+
+@end
---
+++ SendSMS/SendSMS.m
@@ -0,0 +1,13 @@
+//
+//  SendSMS.m
+//  SendSMS
+//
+//  Created by Trevor Porter on 7/14/16.
+//  Copyright © 2016 Trevor Porter. All rights reserved.
+//
+
+#import "SendSMS.h"
+
+@implementation SendSMS
+
+@end
