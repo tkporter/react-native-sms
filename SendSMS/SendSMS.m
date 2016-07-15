b3c9602f707529603f1a9d9950861bdf8523a47a
Trevor Porter <typorter@godaddy.com>
2016-07-15 15:10:46 -0700

    Fixed .m file

---
+++ SendSMS/SendSMS.m
@@ -0,0 +1,63 @@
+//
+//  SendSMS.m
+//  SendSMS
+//
+//  Created by Trevor Porter on 7/13/16.
+
+
+#import "SendSMS.h"
+#import "AppDelegate.h"
+#import "RCTConvert.h"
+
+@implementation SendSMS
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(send:(NSDictionary *)options :(RCTResponseSenderBlock)callback)
+{
+    _callback = callback;
+    MFMessageComposeViewController *messageController = [[MFMessageComposeViewController alloc] init];
+    if([MFMessageComposeViewController canSendText])
+    {
+
+        NSString *body = options[@"body"];
+        NSArray *recipients = options[@"recipients"];
+
+        if (body) {
+          messageController.body = body;
+        }
+
+        if (recipients) {
+          messageController.recipients = recipients;
+        }
+
+        messageController.messageComposeDelegate = self;
+        rootView = [UIApplication sharedApplication].keyWindow.rootViewController;
+        [rootView presentViewController:messageController animated:YES completion:nil];
+    }
+}
+
+-(void) messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result {
+    bool completed = NO, cancelled = NO, error = NO;
+    switch (result) {
+        case MessageComposeResultSent:
+            completed = YES;
+            break;
+        case MessageComposeResultCancelled:
+            cancelled = YES;
+            break;
+        default:
+            error = YES;
+            break;
+    }
+    _callback(@[@(completed), @(cancelled), @(error)]);
+
+    [controller dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
