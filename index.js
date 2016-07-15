87b185344d9e9bf9c902b4b0a72eb7bfe53d9a0a
Trevor Porter <typorter@godaddy.com>
2016-07-15 11:12:26 -0700

    Fix export statement

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-import { NodeModules } from 'react-native'
-module.exports = NodeModules.SendSMS;
+import { NativeModules } from 'react-native'
+module.exports = NativeModules.SendSMS;
