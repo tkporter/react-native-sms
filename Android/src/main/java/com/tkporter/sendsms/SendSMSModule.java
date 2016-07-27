9b8c541d7ef2bf491101ebd72a35cf994314a2c7
Trevor Porter <typorter@godaddy.com>
2016-07-27 10:26:34 -0700

    Added recipient functionality for Android

--- Android/src/main/java/com/tkporter/sendsms/SendSMSModule.java
+++ Android/src/main/java/com/tkporter/sendsms/SendSMSModule.java
@@ -8,6 +8,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.Callback;
 
 public class SendSMSModule extends ReactContextBaseJavaModule implements ActivityEventListener {
@@ -49,9 +50,26 @@ public class SendSMSModule extends ReactContextBaseJavaModule implements Activit
             new SendSMSObserver(reactContext, this, options).start();
 
             String body = options.hasKey("body") ? options.getString("body") : "";
+            ReadableArray recipients = options.hasKey("recipients") ? options.getArray("recipients") : null;
 
             Intent sendIntent = new Intent(Intent.ACTION_VIEW);
             sendIntent.putExtra("sms_body", body);
+
+            //if recipients specified
+            if (recipients != null) {
+                //Samsung for some reason uses commas and not semicolons as a delimiter
+                String separator = "; ";
+                if(android.os.Build.MANUFACTURER.equalsIgnoreCase("Samsung")){
+                    separator = ", ";
+                }
+                String recipientString = "";
+                for (int i = 0; i < recipients.size(); i++) {
+                    recipientString += recipients.getString(i);
+                    recipientString += separator;
+                }
+                sendIntent.putExtra("address", recipientString);
+            }
+
             sendIntent.setType("vnd.android-dir/mms-sms");
             reactContext.startActivityForResult(sendIntent, 1, sendIntent.getExtras());
         } catch (Exception e) {
