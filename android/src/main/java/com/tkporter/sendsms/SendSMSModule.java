513ec19b1cf6353031a98564b66ce3e68ad949bf
kreistad <kenneth.reistad@gmail.com>
2017-10-16 13:36:35 +0200

    Update SendSMSModule.java

--- android/src/main/java/com/tkporter/sendsms/SendSMSModule.java
+++ android/src/main/java/com/tkporter/sendsms/SendSMSModule.java
@@ -90,8 +90,8 @@ public class SendSMSModule extends ReactContextBaseJavaModule implements Activit
                     recipientString += recipients.getString(i);
                     recipientString += separator;
                 }
-                if (recipentString.length() > 2)
-                    recipientString = recipientString.substring(0,recipentString.length()-2);
+                if (recipientString.length() > 2)
+                    recipientString = recipientString.substring(0,recipientString.length()-2);
                 sendIntent.putExtra("address", recipientString);
             }
 
